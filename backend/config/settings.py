1135b781d55254c5501c49822fe7d8a8b5af990a
samuelc40 <samueljoseph2k19@gmail.com>
2026-09-18 12:04:40 +0530

    fix: move database configuration to environment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -142,11 +142,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "toystore",
-        "USER": "postgres",
-        "PASSWORD": "M9803fds@",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
